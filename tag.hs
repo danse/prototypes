d3db65b0e0b7e63341c3afa83ea1cfafa23c47a9
danse <f.occhipinti@gmail.com>
2017-09-28 23:05:21 +0200

    add a `--remove` flag and proper option parsing

--- tag.hs
+++ tag.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE RecordWildCards #-}
 {-
     
   tag a-1 a-2 a
@@ -39,24 +40,38 @@ import System.Environment (getArgs)
 import System.Directory (createDirectoryIfMissing, copyFile, removeFile, getCurrentDirectory)
 import System.FilePath.Posix (takeFileName)
 import System.Posix.Files (createLink)
+import Data.Monoid( (<>) )
+import Control.Applicative( some )
+import Options.Applicative
 
 link file dir = do
   createDirectoryIfMissing True dir
   createLink file (dir ++ "/" ++ (takeFileName file))
 
+data Options = Options {
+  remove :: Bool,
+  args :: [String]
+  }
+
 singleTag tag file = do
   curr <- getCurrentDirectory
   link file (curr ++ "/tags/" ++ tag ++ "/")
 
-tag :: [String] -> IO ()
-tag args = 
+optionParser :: Parser Options
+optionParser = Options
+               <$> switch (long "remove" <> short 'r')
+               <*> some (argument str (metavar "<file1> <file2> ... <tag>"))
+
+tag :: Options -> IO ()
+tag Options {..} = 
   let files = init args
       tag = last args
   in do
     sequence (map (singleTag tag) files)
+    if remove then sequence (map removeFile files) else pure [()]
     return ()
 
-main = do
-    args <- getArgs
-    tag args
+optionParserInfo :: ParserInfo Options
+optionParserInfo = info optionParser fullDesc
 
+main = execParser optionParserInfo >>= tag
