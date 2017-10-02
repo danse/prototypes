b8f336b898ccd8847275159d5113efa96a47ca05
danse <f.occhipinti@gmail.com>
2017-10-02 11:09:14 +0200

    Mo 2. Okt 11:09:14 CEST 2017

--- functions.sh
+++ functions.sh
@@ -3,5 +3,5 @@
 hakyllPostTitle () { echo $(date -Idate)-post-${1:-1}.md; }
 gitSave () { git commit -am "$(date)"; }
 fileAndForget () { gitSave && git push ; clear; }
-shameless () { touch "$*" && git add "$*" && fileAndForget; }
-shy () { touch "$*" && clear; }
+clay () { touch "$*" && git add "$*" && fileAndForget; }
+clayShy () { touch "$*" && clear; }
