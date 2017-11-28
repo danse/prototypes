d73dc1c6f147e32ef20841964d114981c3316b0b
danse <f.occhipinti@gmail.com>
2017-11-28 20:07:30 +0100

    Di 28. Nov 20:07:30 CET 2017

--- functions.sh
+++ functions.sh
@@ -1,7 +1,7 @@
 # install by adding a line like `source ~/prototypes/functions.sh` to
 # your `~/.bashrc`
 hakyllPostTitle () { echo $(date -Idate)-post-${1:-1}.md; }
-gitSave () { git commit -am "$(date)"; }
+gitSave () { git add -A && git commit -m "$(date)"; }
 fileAndForget () { gitSave && git push ; clear; }
 clay () { touch "$*" && git add "$*" && fileAndForget; }
 clayShy () { touch "$*" && clear; }
