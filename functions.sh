a39aa3d08148d31c96ed1b2ba9f76dafabf945fb
danse <f.occhipinti@gmail.com>
2018-03-06 11:24:45 +0100

    Di 6. Mär 11:24:45 CET 2018

--- functions.sh
+++ functions.sh
@@ -6,6 +6,6 @@ fileAndForget () { gitSave && git push ; clear; }
 clay () { touch "$*" && git add "$*" && fileAndForget; }
 clayShy () { touch "$*" && clear; }
 step () { ls -1t; }
-# easily grep on file names
-gaga () { find -iname "*$@*"; }
+# look everywhere in small repos like rotterdam
+gaga () { find -iname "*$@*"; grep -Rli "$@" *; }
 allGit () { ls -d */.git | while read d; do d="${d%/.git}"; cd $d; git $@; cd ../; done }
