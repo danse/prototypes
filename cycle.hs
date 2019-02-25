7a7ceedb3378447f0baa06915476fd593074970d
danse <f.occhipinti@gmail.com>
2019-02-25 13:28:49 +0100

    more flexible use of `cycle`

--- cycle.hs
+++ cycle.hs
@@ -1,8 +1,23 @@
+{-# LANGUAGE NamedFieldPuns #-}
 import Web.Browser (openBrowser)
 import System.Environment (getArgs)
 import Network.URI (isURI)
 import Control.Monad (void)
 import Prototypes (fileApply)
+import Options.Applicative
+import Data.Semigroup ((<>))
+
+data Options = Options { fileNames :: [String], times :: Int }
+
+options :: Parser Options
+options = Options
+          <$> some (argument str (metavar "FILES..."))
+          <*> option auto
+          (long "times"
+           <> help "how many elements do you want to cycle?"
+           <> showDefault
+           <> value 1
+           <> metavar "TIMES")
 
 showOrBrowse s = if isURI s
                  then void (openBrowser s)
@@ -18,7 +33,20 @@ popAndShift :: String -> (String, String)
 popAndShift contents = (unlines (shift l), headOrError l)
   where l = lines contents
 
-main = do
-  [fileName] <- getArgs
+cycle_ :: String -> Int -> IO ()
+cycle_ fileName index = do
   popped <- fileApply fileName popAndShift
   showOrBrowse popped
+
+-- every filename appears in a combination
+combinations (Options { fileNames, times})
+ | l < times = zip c [1..times]
+ | otherwise = zip c [1..l]
+ where l = length fileNames
+       c = cycle fileNames
+
+main = do
+  user <- execParser (info (options <**> helper) fullDesc)
+  sequence (map2 cycle_ (combinations user))
+ where map2 :: (a -> b -> c) -> [(a,b)] -> [c]
+       map2 f l = map (\ (a,b) -> f a b) l
