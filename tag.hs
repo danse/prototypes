7cefa8f3e822d76f3c792cc565127c1c9f633983
danse <f.occhipinti@gmail.com>
2019-12-06 20:33:10 +0100

    improvements to the interface

--- tag.hs
+++ tag.hs
@@ -1,11 +1,12 @@
 {-# LANGUAGE RecordWildCards #-}
 import System.Environment (getArgs)
 import System.Directory (createDirectoryIfMissing, copyFile, removeFile, getCurrentDirectory)
-import System.FilePath.Posix (takeFileName)
+import System.FilePath.Posix (takeFileName, splitPath)
 import System.Posix.Files (createLink)
 import Data.Monoid( (<>) )
 import Control.Applicative( some )
 import Options.Applicative
+import Prototypes (takePathEnd)
 
 link file dir = do
   createDirectoryIfMissing True dir
@@ -13,6 +14,7 @@ link file dir = do
 
 data Options = Options {
   remove :: Bool,
+  tagPath :: FilePath,
   args :: [String]
   }
 
@@ -23,12 +25,13 @@ singleTag tag file = do
 optionParser :: Parser Options
 optionParser = Options
                <$> switch (long "remove" <> short 'r')
+               <*> strOption (long "tag" <> short 't') -- you can use this option with an existing tag to benefit from prefix completion
                <*> some (argument str (metavar "<file1> <file2> ... <tag>"))
 
 tag :: Options -> IO ()
 tag Options {..} = 
   let files = init args
-      tag = last args
+      tag = takePathEnd tagPath
   in do
     sequence (map (singleTag tag) files)
     if remove then sequence (map removeFile files) else pure [()]
