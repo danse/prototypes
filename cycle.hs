e2720ba577073bdbb36baa08d94f4dcdfa9f7734
danse <f.occhipinti@gmail.com>
2020-10-24 19:19:33 +0200

    improve `cycle` with the option to force plain item handling

--- cycle.hs
+++ cycle.hs
@@ -1,27 +1,22 @@
 {-# LANGUAGE NamedFieldPuns #-}
 import Web.Browser (openBrowser)
-import System.Environment (getArgs)
 import Network.URI (isURI)
 import Control.Monad (void)
 import Prototypes (fileApply)
 import Options.Applicative
 import Data.Semigroup ((<>))
 
-data Options = Options { fileNames :: [String], times :: Int }
+data Options = Options {
+  fileNames :: [String],
+  times :: Int,
+  plain :: Bool
+  }
 
 options :: Parser Options
 options = Options
           <$> some (argument str (metavar "FILES..."))
-          <*> option auto
-          (long "times"
-           <> help "how many elements do you want to cycle?"
-           <> showDefault
-           <> value 1
-           <> metavar "TIMES")
-
-showOrBrowse s = if isURI s
-                 then void (openBrowser s)
-                 else print s
+          <*> option auto (long "times" <> help "how many elements do you want to cycle?" <> showDefault <> value 1 <> metavar "TIMES")
+          <*> option auto (long "plain" <> help "print also web addresses rather than opening with a browser" <> showDefault <> value False)
 
 headOrError [] = "the file is empty"
 headOrError o  = head o
@@ -33,20 +28,23 @@ popAndShift :: String -> (String, String)
 popAndShift contents = (unlines (shift l), headOrError l)
   where l = lines contents
 
-cycle_ :: String -> Int -> IO ()
-cycle_ fileName index = do
+cycle_ :: Bool -> String -> Int -> IO ()
+cycle_ plain fileName index = do
   popped <- fileApply fileName popAndShift
-  showOrBrowse popped
+  if isURI popped && not plain then void (openBrowser popped)
+    else putStrLn popped
+  where withPref l = "https://" ++ l
 
+combinations :: Options -> [(String, Int)]
 -- every filename appears in a combination
-combinations (Options { fileNames, times})
+combinations (Options { fileNames, times })
  | l < times = zip c [1..times]
  | otherwise = zip c [1..l]
  where l = length fileNames
        c = cycle fileNames
 
 main = do
-  user <- execParser (info (options <**> helper) fullDesc)
-  sequence (map2 cycle_ (combinations user))
- where map2 :: (a -> b -> c) -> [(a,b)] -> [c]
-       map2 f l = map (\ (a,b) -> f a b) l
+  opt <- execParser (info (options <**> helper) fullDesc)
+  sequence (map2 (cycle_ (plain opt)) (combinations opt))
+  where map2 :: (a -> b -> c) -> [(a,b)] -> [c]
+        map2 f l = map (\ (a,b) -> f a b) l
